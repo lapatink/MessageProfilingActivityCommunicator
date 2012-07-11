b8c288363e819f4163ab005f84ae78d0078cb7f6
Michael Donato <donatom@wit.edu>
2012-07-10 21:06:34 -0400

    Force window focus class
    This breaks completely when you have another window open. (open file
    dialog, error messages). I don't recommend using it yet.

---
+++ MPacApplication/MPacApplication/Focus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MPacApplication
+{
+    class Focus
+    {
+        Form f;
+        Timer t;
+        public bool active = true;
+
+        public Focus(Form f)
+        {
+            this.f = f;
+            this.f.TopMost = true;
+
+            t = new Timer();
+            t.Interval = 1;
+            t.Tick += new EventHandler(t_Tick);
+
+            this.f.Activated += new EventHandler(f_Activated);
+            this.f.Deactivate += new EventHandler(f_Deactivate);
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            if (active)
+                f.Activate();
+        }
+        private void f_Activated(object sender, EventArgs e)
+        {
+            t.Stop();
+        }
+        private void f_Deactivate(object sender, EventArgs e)
+        {
+            t.Start();
+        }
+    }
+}
