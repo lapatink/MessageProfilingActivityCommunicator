ce0059f156f593ce6cbd52b6e689545758e41915
Michael Donato <donatom@wit.edu>
2012-07-12 21:21:15 -0400

    Added table to connection string
    It's concatenated on the end of the normal SQL connection string. Also
    fixed namespace and added a read-only isAdministrator property.

--- MPacApplication/MPacApplication/Configuration.cs
+++ MPacApplication/MPacApplication/Configuration.cs
@@ -4,19 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 
-namespace MPACApplication
+namespace MPacApplication
 {
-    class Configuration //I might make this static later
+    class Configuration
     {
+        #region Member Variables
         private const int CONNECTION_LIMIT = 10;
         private string connectionString = string.Empty;
         private string filename = "config.xml";
         private Connection[] connections = new Connection[CONNECTION_LIMIT];
         private int count = -1;
+        private bool isAdmin = false;
+        /// <summary>
+        /// The number of database connections found in the configuration files. Default value is -1.
+        /// </summary>
         public int Count
         {
             get { return count; }
         }
+        /// <summary>
+        /// Is the user authorized as an administrator?
+        /// </summary>
+        public bool IsAdministrator
+        {
+            get { return isAdmin; }
+        }
+        #endregion
 
         public Configuration()
         {
@@ -26,6 +39,11 @@ namespace MPACApplication
         {
             this.filename = filename;
         }
+
+        /// <summary>
+        /// Reads all relevant configuration files. The default file is config.xml in the current directory.
+        /// </summary>
+        /// <returns>Returns an array of connection strings with the table name concatenated at the end.</returns>
         public string[] Read()
         {
             XmlReader reader = XmlReader.Create(filename);
@@ -56,6 +74,9 @@ namespace MPACApplication
 
                             connections[count] = c;
                         }
+                        break;
+                    case "admin":
+                        isAdmin = (reader.ReadInnerXml() == "true" ? true : false);
                         break;
                     case "connection":
                         count++;
@@ -112,7 +133,7 @@ namespace MPACApplication
 
             public string getConnectionString()
             {
-                return "Data Source=" + source + ";Initial Catalog=" + catalog + ";User ID=" + user + ";Password=" + password + ";";
+                return "Data Source=" + source + ";Initial Catalog=" + catalog + ";User ID=" + user + ";Password=" + password + ";" + table;
             }
         }
 
